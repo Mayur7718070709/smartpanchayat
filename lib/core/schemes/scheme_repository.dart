83afbe3f7458c0eba94881054ea4be7788526f0a
Mayur <mayurresh.patil0707@gmail.com>
2026-08-24 20:19:22 +0530

    feat: connect schemes to FastAPI

--- lib/core/schemes/scheme_repository.dart
+++ lib/core/schemes/scheme_repository.dart
@@ -1,10 +1,16 @@
 import '../network/api_client.dart';
+import '../../models/scheme_model.dart';
 
 class SchemeRepository {
   const SchemeRepository(this._apiClient);
 
   final ApiClient _apiClient;
 
-  Future<void> checkAvailability() =>
-      _apiClient.get<void>('/api/v1/schemes', decode: (_) {});
+  Future<List<SchemeModel>> list({int limit = 100}) => _apiClient.get(
+    '/api/v1/schemes',
+    queryParameters: {'limit': limit},
+    decode: (data) => ((data as Map<String, dynamic>)['items'] as List)
+        .map((item) => SchemeModel.fromApi(item as Map<String, dynamic>))
+        .toList(),
+  );
 }
